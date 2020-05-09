c2e41556061128f1c4e658be89f061ba4280e91a
jasminecao <jasminecao03@gmail.com>
2020-05-09 13:37:15 -0400

    user schema

---
+++ models/user.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose')
+
+const UserSchema = mongoose.Schema({
+  name: {
+    type: String,
+    require: true
+  },
+  username: {
+    type: String,
+    require: true
+  },
+  password: {
+    type: String,
+    require: true
+  }
+});
+
+module.exports = User = mongoose.model('UserSchema', UserSchema)
