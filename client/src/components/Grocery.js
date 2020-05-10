88789260f519b5ad9bd51efe26e78f9b5c7460c2
jasminecao <jasminecao03@gmail.com>
2020-05-10 15:07:43 -0400

    finish basic grocery list

--- client/src/components/Grocery.js
+++ client/src/components/Grocery.js
@@ -14,6 +14,11 @@ const Grocery = () => {
     if (e.key === 'Enter') {
       addItemAtIndex(e, i);
     }
+
+    if (e.key === 'Backspace' && items[i].name === '') {
+      e.preventDefault();
+      return removeItemAtIndex(i);
+    }
   }
 
   function addItemAtIndex(e, i) {
@@ -28,6 +33,26 @@ const Grocery = () => {
     }, 0);
   }
 
+  function updateItemAtIndex(e, i) {
+    const newItems = [...items];
+    newItems[i].name = e.target.value;
+    setItems(newItems);
+  }
+
+  function removeItemAtIndex(i) {
+    if (i === 0 && items.length === 1) return;
+    setItems(items => items.slice(0, i).concat(items.slice(i + 1, items.length)));
+    setTimeout(() => {
+      document.forms[0].elements[i - 1].focus();
+    }, 0);
+  }
+
+  function toggleItemCompleteAtIndex(index) {
+    const tempItems = [...items];
+    tempItems[index].isCompleted = !tempItems[index].isCompleted;
+    setItems(tempItems);
+  }
+
   function addItem() {
     const copyArray = items.slice();
     copyArray.push("");
@@ -40,15 +65,24 @@ const Grocery = () => {
       <form className="todo-list">
         <ul>
           {items.map((item, i) => (
-            <div className="todo">
-              <div className="checkbox" />
-              <input className="itemInput" type="text" value={item.name} onKeyDown={e => handleKeyDown(e, i)} />
+            <div className={`todo ${item.isCompleted && 'todo-is-completed'}`}>
+              <div className="checkbox" onClick={() => toggleItemCompleteAtIndex(i)}>
+                {item.isCompleted && (
+                  <span style={{color: "white"}}>✔</span>
+                )}
+              </div>
+              <input 
+                className={`itemInput ${item.isCompleted && 'item-is-completed'}`}
+                type="text" 
+                value={item.name} 
+                onKeyDown={e => handleKeyDown(e, i)} 
+                onChange={e => updateItemAtIndex(e, i)}/>
             </div>
           ))
           }
         </ul>
       </form>
-      <button className="addButton" onClick={() => addItem()}>+ add new item</button>
+      <button className="addButton" onClick={e => addItemAtIndex(e, items.length - 1)}>+ add new item</button>
     </div>
   )
 }
